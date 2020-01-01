debc61565f04edcfe63ee978b5e6cda7f9f3096e
JM <jdreher15@gmail.com>
2020-01-01 11:22:51 -0800

    big clean-up, never updated all the different changes, updating now.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import RetroHeadliner from './components/RetroHeadliner/RetroHeadliner';
-import RetroLinkedin from './RetroLinkedin';
-import RetroProjects from './RetroProjects';
-import RetroTechLinks from './RetroTechLinks';
-import './style.css';
+import RetroLinkedin from './components/RetroLinkedin/RetroLinkedin';
+import RetroProjects from './components/RetroProjects/RetroProjects';
+import './index';
 
 function App() {
   return (
     <div className="main-app">
-    <div className="component-container">
-      <RetroHeadliner className="header"></RetroHeadliner>
-      <div className="middle-main-split">
-        <RetroLinkedin className="linked"></RetroLinkedin>
-        <RetroProjects className="projects"></RetroProjects>
-      </div>
-      <RetroTechLinks className="tech"></RetroTechLinks>
-    </div>
+      <RetroHeadliner />
+      <RetroLinkedin/>
+      <RetroProjects />
   </div>
   );
 }
---
+++ src/components/RetroLinkedin/RetroLinkedin.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import './RetroLinkedin.css';
+
+export const RetroLinkedin = ({ mainImage }) => {
+mainImage = 'https://s3-us-west-1.amazonaws.com/marmot-clone-start/me_cutout.png';
+  return (
+    <div className="banner-container">
+      <img src={mainImage} alt='me vice'/>
+
+      <div className='button-holder'>
+        <div className="below-button">
+          <a href="https://www.linkedin.com/in/jonmichaeldreher/" target="_blank" rel="noopener noreferrer">
+            <button className="btn cta">
+            <span >LinkedIn</span>
+            </button>
+          </a>
+        </div>
+
+        <div className="below-button">
+          <a href="https://drive.google.com/open?id=0B3ROmtwfUntWbE9aTE00TXE4b01kNXFqcTNyMHNNcC1CX2o0" target="_blank" rel="noopener noreferrer">
+            <button className="btn cta">
+              <span style={{fontSize: '2.5rem'}}>Resume</span>
+            </button> 
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RetroLinkedin;
