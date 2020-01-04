3afc5ec4824852695e0411a02101dbda7f9b6fe5
JM <jdreher15@gmail.com>
2020-01-03 19:40:39 -0800

    fix tiny image man.

--- src/components/RetroLinkedin/RetroLinkedin.js
+++ src/components/RetroLinkedin/RetroLinkedin.js
@@ -11,7 +11,7 @@ mainImage = 'https://s3-us-west-1.amazonaws.com/marmot-clone-start/me_cutout.png
         <div className="below-button">
           <a href="https://www.linkedin.com/in/jonmichaeldreher/" target="_blank" rel="noopener noreferrer">
             <button className="btn cta">
-            <span style={{fontSize: '2.5rem'}}>LinkedIn</span>
+            <span className="button-font">LinkedIn</span>
             </button>
           </a>
         </div>
@@ -19,7 +19,7 @@ mainImage = 'https://s3-us-west-1.amazonaws.com/marmot-clone-start/me_cutout.png
         <div className="below-button">
           <a href="https://drive.google.com/open?id=0B3ROmtwfUntWbE9aTE00TXE4b01kNXFqcTNyMHNNcC1CX2o0" target="_blank" rel="noopener noreferrer">
             <button className="btn cta">
-              <span style={{fontSize: '2.5rem'}}>Resume</span>
+              <span className="button-font">Resume</span>
             </button> 
           </a>
         </div>
--- src/components/RetroProjects/RetroProjects.js
+++ src/components/RetroProjects/RetroProjects.js
@@ -6,17 +6,17 @@ export const RetroProjects = () => {
     <div className="main-pro-wrapper">
       <div className="btn">
           <a href="https://youtu.be/QZPfwbL5fHs" target="_blank" rel="noopener noreferrer">
-              <span style={{fontSize: '2.5rem'}}>Virtual Library</span>
+              <span className="button-font">Virtual Library</span>
           </a>
       </div>
       <div className="btn">
       <a href="https://cudemo.goalexandria.com/offline" target="_blank" rel="noopener noreferrer">
-          <span style={{fontSize: '2.5rem'}}>Offline Data Entry</span>
+          <span className="button-font">Offline Data Entry</span>
         </a>
       </div>
       <div className="btn">
       <a href="https://cranky-hoover-5a469d.netlify.com/" target="_blank" rel="noopener noreferrer">
-          <span style={{fontSize: '2.5rem'}}>Outdoor Gear</span>
+          <span className="button-font">Outdoor Gear</span>
         </a>
       </div>
   </div>
