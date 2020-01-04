f6f93a4ddd6d2d1db85933f3c2a6a3a1e2819ab0
JM <jdreher15@gmail.com>
2020-01-03 19:44:46 -0800

    update tab tab.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Jon-Michael Dreher in header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = getByText(/Jon-Mchael Dreher/i);
+  expect(header).toBeInTheDocument();
 });
